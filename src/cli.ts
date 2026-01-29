6d92d06543a959d255c74a40b3059640c7e43bb6
jmsengr <batayolajakejames@gmail.com>
2026-01-29 12:23:05 +0800

    UPDATE: cli logic

--- src/cli.ts
+++ src/cli.ts
@@ -10,31 +10,25 @@ dotenv.config();
 const flag: string[] = process.argv.slice(2);
 
 if (!flag.length) {
-    console.log(kleur.red("No flag provided"));
-    console.log(kleur.underline("start") + " - Start the Broadcast Server");
-    console.log(kleur.underline("connect") + " - Connect to an existing Broadcast Server");
-}
-
-if (flag.length > 1) {
-    console.log(kleur.red("Only one flag is allowed at a time"));
-    console.log(kleur.underline("start") + " - Start the Broadcast Server");
-    console.log(kleur.underline("connect") + " - Connect to an existing Broadcast Server");
-}
-
-if (flag[0] !== "start" && flag[0] !== "connect") {
-    console.log(kleur.red("Invalid flag provided"));
-    console.log(kleur.underline("start") + " - Start the Broadcast Server");
-    console.log(kleur.underline("connect") + " - Connect to an existing Broadcast Server");
-}
-
-if (flag[0] === "start") {
-    isPortTaken(Number(process.env.PORT) || 3000, (taken: boolean) => {
-        if (taken) {
-            console.log(kleur.red(`Server is already running on port ${process.env.PORT || 3000}`));
-            console.log(`Use ` + kleur.underline("connect") + ` to connect to the existing server.`);
-            process.exit(1);
-        } else {
-            startServer();
-        }
-    });
+	console.log(kleur.red("No flag provided"));
+	console.log(kleur.underline("start") + " - Start the Broadcast Server");
+	console.log(kleur.underline("connect") + " - Connect to an existing Broadcast Server");
+} else if (flag.length > 1) {
+	console.log(kleur.red("Only one flag is allowed at a time"));
+	console.log(kleur.underline("start") + " - Start the Broadcast Server");
+	console.log(kleur.underline("connect") + " - Connect to an existing Broadcast Server");
+} else if (flag[0] !== "start" && flag[0] !== "connect") {
+	console.log(kleur.red("Invalid flag provided"));
+	console.log(kleur.underline("start") + " - Start the Broadcast Server");
+	console.log(kleur.underline("connect") + " - Connect to an existing Broadcast Server");
+} else if (flag[0] === "start") {
+	isPortTaken(Number(process.env.PORT) || 3000, (taken: boolean) => {
+		if (taken) {
+			console.log(kleur.red(`Server is already running on port ${process.env.PORT || 3000}`));
+			console.log(`Use ` + kleur.underline("connect") + ` to connect to the existing server.`);
+			process.exit(1);
+		} else {
+			startServer();
+		}
+	});
 }
